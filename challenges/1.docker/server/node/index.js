c949a93cecfe87403a9c72fdc911b6f5abab4e36
Maicon Silva <maiconrs95@gmail.com>
2023-02-06 19:16:37 -0300

    feat: added home page

--- challenges/1.docker/server/node/index.js
+++ challenges/1.docker/server/node/index.js
@@ -8,43 +8,71 @@ const connection = mysql.createConnection({
     password: "root",
     database: "users",
 });
-const port = 3000;
 
-const User = {
-    create: (user = "") => {
-        if (!user) return;
-        connection.query(`INSERT INTO people(name) values('${user}')`);
-        connection.end();
-    },
-    get: () => {
-        const users = connection.query("SELECT * FROM people");
-        connection.end();
-    },
-};
-
-app.use((req, res, next) => {
+app.use((req, _, next) => {
     console.log(req.method, req.url);
     next();
 });
 
-const Home = () => {
-    const users = User.get();
-    console.log(users);
-    return `
-        <h1>Full Cycle Rocks!</h1>
-        <form method="get">
-        <label for="name">
-            <input type="text" name="name" />
-         </label>
-        </form>
-    `;
+const User = {
+    create: (user = "") => {
+        if (!user) return;
+        connection.query(
+            `INSERT INTO people(name) values('${user}')`,
+            (error) => {
+                if (error) throw error;
+            }
+        );
+    },
+    get: () =>
+        new Promise(function (resolve, reject) {
+            connection.query("SELECT * FROM people", (error, results) => {
+                if (error) reject(error);
+                resolve(JSON.parse(JSON.stringify(results)));
+            });
+        }),
 };
 
-app.get("/", (req, res) => {
-    User.create(req.query?.name);
-    res.send(Home());
+const Home = async () => {
+    const users = await User.get();
+    const HAS_USERS = Boolean(users.length);
+
+    const Element = `
+       <h1>Full Cycle Rocks!</h1>
+        <form method="post">
+        <label for="name">
+            <input type="text" name="name" />
+        </label>
+        </form>
+        ${
+            HAS_USERS
+                ? `
+                    <ul>
+                        ${users
+                            .map((user) => `<li>${user.id} - ${user.name}</li>`)
+                            .join("")}
+                    </ul>
+                `
+                : ""
+        }
+    `;
+
+    return Element;
+};
+
+app.get("/", async (req, res) => {
+    const Page = await Home();
+    res.send(Page);
 });
 
-app.listen(3000, () => {
-    console.log(`app is running on port: ${port}`);
+app.post("/", async (req, res) => {
+    User.create(req.body?.name);
+    const Page = await Home();
+    res.send(Page);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`app is running on port: ${PORT}`);
 });
