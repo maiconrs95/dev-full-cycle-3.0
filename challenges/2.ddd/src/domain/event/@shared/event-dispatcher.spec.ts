a25c56cc9f86f189f2dfd56197a1475b0275d069
Maicon Silva <maiconrs95@gmail.com>
2023-06-27 17:55:44 -0300

    feat: added some tests

--- challenges/2.ddd/src/domain/event/@shared/event-dispatcher.spec.ts
+++ challenges/2.ddd/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,5 +1,6 @@
 import EventDispatcher from '../../@shared/event/event-dispatcher'
 import SendEmailWhenProductIsCreatedHandler from '../../product/event/handler/send-email-when-product-is-created.handler';
+import ProductCreatedEvent from "../../product/event/product-created.event";
 
 describe('Domain events tests', () => {
     it('should register an event handler', () => {
@@ -42,4 +43,27 @@ describe('Domain events tests', () => {
             eventDispatcher.getEventHandlers["ProductCreatedEvent"]
         ).toBeUndefined();
     });
+
+    it('should notify all events handlers', () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(
+            eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+        ).toMatchObject(eventHandler);
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            description: "Product 1 description",
+            price: 10.0,
+        });
+
+        // Quando o notify for executado o SendEmailWhenProductIsCreatedHandler.handle() deve ser chamado
+        eventDispatcher.notify(productCreatedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+    })
 })
