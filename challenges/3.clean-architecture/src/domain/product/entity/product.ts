576065668cb0ef877fc6d7e55dc2f010e10b40f4
Maicon Silva <maiconrs95@gmail.com>
2024-03-26 10:08:38 -0300

    fix: some review changes

--- challenges/3.clean-architecture/src/domain/product/entity/product.ts
+++ challenges/3.clean-architecture/src/domain/product/entity/product.ts
@@ -37,6 +37,23 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate() {
-    ProductValidatorFactory.create().validate(this);
+    if (this._id.length === 0) {
+      this.notification.addError({
+        context: 'product',
+        message: 'Id is required',
+      });
+    }
+    if (this._name.length === 0) {
+      this.notification.addError({
+        context: 'product',
+        message: 'Name is required',
+      });
+    }
+    if (this._price < 0) {
+      this.notification.addError({
+        context: 'product',
+        message: 'Price must be greater than zero',
+      });
+    }
   }
 }
