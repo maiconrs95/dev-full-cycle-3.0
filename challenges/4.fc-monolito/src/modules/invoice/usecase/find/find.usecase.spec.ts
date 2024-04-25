6d4d8cc8b927b150a60f48e729cc135b504f102b
Maicon Silva <maiconrs95@gmail.com>
2024-04-24 22:22:20 -0300

    feat(fc-monolito): Add Invoice find use case

---
+++ challenges/4.fc-monolito/src/modules/invoice/usecase/find/find.usecase.spec.ts
@@ -0,0 +1,56 @@
+import FindInvoiceUseCase from './find.usecase'
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Address from "../../../@shared/domain/value-object/address";
+import Invoice from '../../domain/invoice.entity';
+
+const invoice = new Invoice({
+    id: new Id('1'),
+    name: 'invoice 1',
+    document: 'document 1',
+    address: new Address(
+        'street 1',
+        'number 1',
+        'complemente 1',
+        'city 1',
+        'stata 1',
+        'zip-code-1',
+    ),
+    items: [
+        {
+            id: 'uuid-item-1',
+            name: 'item 1',
+            price: 100
+        },
+        {
+            id: 'uuid-item-1',
+            name: 'item 1',
+            price: 150
+        }
+    ],
+})
+
+const MockRepository = () => {
+    return {
+        generate: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(invoice)),
+    }
+};
+
+describe("find Invoice Usecase unit test", () => {
+    it("should find a Invoice", async () => {
+        const repository = MockRepository();
+        const usecase = new FindInvoiceUseCase(repository);
+
+        const input = { id: '1' }
+        const result = await usecase.execute(input);
+
+        expect(repository.find).toHaveBeenCalled();
+        expect(result.id).toEqual(input.id);
+        expect(result.name).toEqual(invoice.name);
+        expect(result.document).toEqual(invoice.document);
+        expect(result.address).toEqual(invoice.address);
+        expect(result.items).toEqual(invoice.items);
+        expect(result.total).toEqual(250);
+        expect(result.createdAt).toEqual(invoice.createdAt);
+    });
+});
---
+++ challenges/4.fc-monolito/src/modules/invoice/usecase/find/find.usecase.ts
@@ -0,0 +1,24 @@
+import InvoiceGateway from '../../gateway/invoice.gateway';
+import { FindInvoiceUseCaseInputDTO, FindInvoiceUseCaseOutputDTO } from './find.usecase.dto';
+
+export default class GenerateInvoiceUseCase {
+    private _clientRepository: InvoiceGateway;
+
+    constructor(clientRepository: InvoiceGateway) {
+        this._clientRepository = clientRepository;
+    }
+
+    async execute(input: FindInvoiceUseCaseInputDTO): Promise<FindInvoiceUseCaseOutputDTO> {
+        const result = await this._clientRepository.find(input.id);
+
+        return {
+            id: result.id.id,
+            name: result.name,
+            document: result.document,
+            address: result.address,
+            items: result.items,
+            total: result.items.reduce((prev, curr) => prev + curr.price, 0),
+            createdAt: result.createdAt,
+        }
+    }
+}
