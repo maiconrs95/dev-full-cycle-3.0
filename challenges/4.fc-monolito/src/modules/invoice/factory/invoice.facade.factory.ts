90230bdec68b0e145f9e33cdd1c95da7b0ff33d0
Maicon Silva <maiconrs95@gmail.com>
2024-05-13 20:30:38 -0300

    feat: add invoice factory

---
+++ challenges/4.fc-monolito/src/modules/invoice/factory/invoice.facade.factory.ts
@@ -0,0 +1,17 @@
+import InvoiceFacade from "../facade/invoice.facade";
+import InvoiceRepository from "../repository/invoice.repository";
+import GenerateInvoiceUseCase from "../usecase/generate/generate.usecase";
+import FindInvoiceUseCase from "../usecase/find/find.usecase";
+
+export default class InvoiceFacadeFactory {
+  static create() {
+    const repository = new InvoiceRepository();
+    const findUsecase = new FindInvoiceUseCase(repository);
+    const generateUsecase = new GenerateInvoiceUseCase(repository);
+    const facade = new InvoiceFacade({
+      generateUseCase: generateUsecase,
+      findUsecase: findUsecase,
+    });
+    return facade;
+  }
+}
