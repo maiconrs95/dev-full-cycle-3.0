92eff833be05976afbed9043fbdbb4871ca7c2ba
Maicon Silva <maiconrs95@gmail.com>
2024-08-29 19:09:19 -0300

    feat: added place order use cases

--- challenges/4.fc-monolito/src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ challenges/4.fc-monolito/src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -1,7 +1,92 @@
 import { PlaceOrderInputDto } from './place-order.dto'
 import PlaceOrderUseCase from './place-order.usecase'
 
+const mockDate = new Date(2000, 1, 1)
+
 describe('PlaceOrderUsecase uni test', () => {
+    describe('validateProduct methods', () => {
+        // @ts-expect-error - no params in constructor
+        const placeOrderUsecase = new PlaceOrderUseCase()
+
+        it('should throw an error when products are not valid', async () => {
+            const input: PlaceOrderInputDto = { clientId: '0', products: [] }
+
+            await expect(placeOrderUsecase['validateProducts'](input)).rejects.toThrowError(
+                new Error('No products selected')
+            )
+        })
+
+        it('should throw an error when products are out of stock', async () => {
+            const mockProductFacade = {
+                checkStock: jest.fn(( { productId }: { productId: string } ) => {
+                    return Promise.resolve({
+                        productId,
+                        stock: productId === '1' ? 0 : 1
+                    })
+                })
+            }
+
+            // @ts-expect-error
+            placeOrderUsecase['_productFacade'] = mockProductFacade
+
+            let input: PlaceOrderInputDto = {
+                clientId: '0',
+                products: [{productId: '1'}]
+            }
+
+            await expect(placeOrderUsecase['validateProducts'](input)).rejects.toThrowError(
+                new Error('Product 1 is not available in stock')
+            )
+
+            input = {
+                clientId: '0',
+                products: [{productId: '0'}, {productId: '1'}],
+            }
+
+            await expect(placeOrderUsecase['validateProducts'](input)).rejects.toThrowError(
+                new Error('Product 1 is not available in stock')
+            )
+            expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(3)
+
+            input = {
+                clientId: '0',
+                products: [{productId: '0'}, {productId: '1'}, {productId: '2'}],
+            }
+
+            await expect(placeOrderUsecase['validateProducts'](input)).rejects.toThrowError(
+                new Error('Product 1 is not available in stock')
+            )
+            expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(5)
+        })
+    })
+
+    describe('getProductTotal method', () => {
+        beforeAll(() => {
+            jest.useFakeTimers('modern')
+            jest.setSystemTime(mockDate)
+        })
+
+        afterAll(() => {
+            jest.useRealTimers()
+        })
+
+        // @ts-expect-error - no params in constructor
+        const placeOrderUseCase = new PlaceOrderUseCase();
+
+        it('should throw erro when product is no found',async () => {
+            const mockCatalogFacade = {
+                find: jest.fn().mockResolvedValue(null)
+            }
+
+            // @ts-expect-error
+            placeOrderUseCase['_catalogFacade'] = mockCatalogFacade;
+
+          await expect(placeOrderUseCase['getProduct']('0')).rejects.toThrowError(
+                new Error('Product not found')
+            )
+        })
+    })
+
     describe('execute method', () => {
         it('should return an order with status pending', async () => {
             const mockClientFacade = {
--- challenges/4.fc-monolito/src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ challenges/4.fc-monolito/src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,12 +1,20 @@
+import Id from '../../../@shared/domain/value-object/id.value-object';
 import UseCaseInterface from '../../../@shared/usecase/use-case.interface';
 import ClientAdmFacadeInterface from '../../../client-adm/facade/client-adm.facade.interface';
+import ProductAdmFacadeInterface from '../../../product-adm/facade/product-adm.facade.interface';
+import StoreCatalogFacadeInterface from '../../../store-catalog/facade/store-catalog.facade.interface';
+import Product from '../../domain/product.entity';
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from './place-order.dto';
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
     private _clientFacade: ClientAdmFacadeInterface;
+    private _productFacade: ProductAdmFacadeInterface
+    private _catalogFacade: StoreCatalogFacadeInterface;
 
-    constructor(_clientFacade: ClientAdmFacadeInterface) {
-        this._clientFacade = _clientFacade;
+    constructor(clientFacade: ClientAdmFacadeInterface, productFacade: ProductAdmFacadeInterface, catalogFacade: StoreCatalogFacadeInterface) {
+        this._clientFacade = clientFacade;
+        this._productFacade = productFacade;
+        this._catalogFacade = catalogFacade;
     }
 
     async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -31,5 +39,30 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
         if (!input.products || input.products.length === 0) {
             throw new Error('No products selected');
         }
+
+        for (const p of input.products) {
+            const product = await this._productFacade.checkStock({
+                 productId: p.productId
+            });
+
+            if (product.stock <= 0) {
+                throw new Error(`Product ${p.productId} is not available in stock`);
+            }
+        }
+    }
+
+    private async getProduct(productId: string): Promise<Product> {
+        const product = await this._catalogFacade.find({ id: productId });
+
+        if (!product) {
+            throw new Error('Product not found');
+        }
+
+        return new Product({
+            id: new Id(product.id),
+            name: product.name,
+            description: product.description,
+            salesPrice: product.salesPrice
+        });
     }
 }
